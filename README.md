4d01e5323116f4b0131f8cc842a9a858250ec3fc
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2024-08-16 21:13:46 +0200

    develop

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# symbol
+# Symbol
+
 Symboltables
+
+Bron https://github.com/Geonovum/TPOD/blob/master/Presentatiemodel/Symbolenbibliotheek%20STOP-TPOD%20v1.1.0.zip
+
+
+## References
+- [jaxb tutorial](https://www.baeldung.com/jaxb)
+
+
+## Maven package
+- https://repository.sonatype.org/
+- https://mvnrepository.com/search
---
+++ src/main/java/com/bsoft/symbol/SymbolApplication.java
@@ -0,0 +1,13 @@
+package com.bsoft.symbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymbolApplication.class, args);
+	}
+
+}
